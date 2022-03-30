c0419e486e9f0429afed0c06298076b2f4e8258c
Mahak5457 <2020uee1312@mnit.ac.in>
2022-03-30 12:14:01 +0530

    Updates

--- _app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/styles.css'
-import '../styles/carousal.css'
-
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
